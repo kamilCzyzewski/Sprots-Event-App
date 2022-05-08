8389a1bdaa85f0ac5830c76803a5e8bf35ff42f6
Kamil Czyżewski <kamil8739@gmail.com>
2022-05-08 16:21:45 +0200

    feat:add userService #4

--- src/main/java/com/event/app/sports/repository/UserRepository.java
+++ src/main/java/com/event/app/sports/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    UserEntity readByUsername(String username);
+    UserEntity findOneByUsername(String username);
 }
